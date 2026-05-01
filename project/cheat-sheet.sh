893df1f01a9ce143f48660dc704c6e8806df5219
Nussipakhyn Khan <tenseizangetsu3@gmail.com>
2026-05-02 02:03:03 +0500

    I hope will work

--- project/cheat-sheet.sh
+++ project/cheat-sheet.sh
@@ -1,9 +1,18 @@
 kubectl patch svc authentik -n authentik --type=merge \
-  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","version":"green"}}}'
+  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","app.kubernetes.io/instance":"authentik-green"}}}'
 
+kubectl scale deployment authentik-blue-server -n authentik --replicas=0
+kubectl scale deployment authentik-blue-worker -n authentik --replicas=0
+kubectl scale deployment authentik-green-server -n authentik --replicas=1
+kubectl scale deployment authentik-green-worker -n authentik --replicas=1
 kubectl get endpoints authentik -n authentik
 
 kubectl patch svc authentik -n authentik --type=merge \
-  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","version":"blue"}}}'
+  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","app.kubernetes.io/instance":"authentik-blue"}}}'
+
+kubectl scale deployment authentik-blue-server -n authentik --replicas=1
+kubectl scale deployment authentik-blue-worker -n authentik --replicas=1
+kubectl scale deployment authentik-green-server -n authentik --replicas=0
+kubectl scale deployment authentik-green-worker -n authentik --replicas=0
 
 kubectl get endpoints authentik -n authentik
