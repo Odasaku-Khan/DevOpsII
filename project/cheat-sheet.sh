e877f0164544ef0f46acea1c71d08222361b9c52
Nussipakhyn Khan <tenseizangetsu3@gmail.com>
2026-05-02 02:46:53 +0500

    california girls

--- project/cheat-sheet.sh
+++ project/cheat-sheet.sh
@@ -1,5 +1,15 @@
-kubectl patch svc authentik -n authentik --type=merge \
-  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","app.kubernetes.io/instance":"authentik-green"}}}'
+kubectl patch svc authentik -n authentik --type='json' -p='[
+  {
+    "op": "replace",
+    "path": "/spec/selector",
+    "value": {
+      "app.kubernetes.io/name": "authentik",
+      "app.kubernetes.io/component": "server",
+      "app.kubernetes.io/instance": "authentik-green"
+    }
+  }
+]'
+
 
 kubectl scale deployment authentik-blue-server -n authentik --replicas=0
 kubectl scale deployment authentik-blue-worker -n authentik --replicas=0
@@ -7,8 +17,18 @@ kubectl scale deployment authentik-green-server -n authentik --replicas=1
 kubectl scale deployment authentik-green-worker -n authentik --replicas=1
 kubectl get endpoints authentik -n authentik
 
-kubectl patch svc authentik -n authentik --type=merge \
-  -p '{"spec":{"selector":{"app.kubernetes.io/name":"authentik","app.kubernetes.io/component":"server","app.kubernetes.io/instance":"authentik-blue"}}}'
+kubectl patch svc authentik -n authentik --type='json' -p='[
+  {
+    "op": "replace",
+    "path": "/spec/selector",
+    "value": {
+      "app.kubernetes.io/name": "authentik",
+      "app.kubernetes.io/component": "server",
+      "app.kubernetes.io/instance": "authentik-blue"
+    }
+  }
+]'
+
 
 kubectl scale deployment authentik-blue-server -n authentik --replicas=1
 kubectl scale deployment authentik-blue-worker -n authentik --replicas=1
