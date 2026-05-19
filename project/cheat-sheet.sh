cde8aa04ead22de757464de167aee7c4f9a009bd
Nussipakhyn Khan <tenseizangetsu3@gmail.com>
2026-05-19 17:06:35 +0500

    disbcjlka

--- project/cheat-sheet.sh
+++ project/cheat-sheet.sh
@@ -6,6 +6,8 @@ kubectl patch svc authentik -n authentik --type='merge' -p='
   "app.kubernetes.io/component":"server",
   "app.kubernetes.io/instance":"authentik-blue"
 }}}'
+kubectl -n demo-app patch svc protected-portal \
+  -p '{"spec":{"selector":{"app":"protected-portal","version":"blue"}}}'
 
 Green active:
 
@@ -15,3 +17,6 @@ kubectl patch svc authentik -n authentik --type='merge' -p='
   "app.kubernetes.io/component":"server",
   "app.kubernetes.io/instance":"authentik-green"
 }}}'
+
+kubectl -n demo-app patch svc protected-portal \
+  -p '{"spec":{"selector":{"app":"protected-portal","version":"green"}}}'
